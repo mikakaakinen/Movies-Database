cdf225d88a9f2518c3e90a9d5dc467a3a38ba5eb
Mika Kaakinen <mika.kaakinen@gmail.com>
2018-04-29 10:23:46 +0300

    Help-6

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 app.get('/',function (req, res) {
-  res.send('Hello world!');
+  res.render('index');
 });
 
 var index = require('./routes/index');
