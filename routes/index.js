92a96439c5fbe3b7d4357393b526e393db19b5d2
Mika Kaakinen <mika.kaakinen@gmail.com>
2018-04-29 12:48:52 +0300

    Help-7

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.send('Hello Application');
   res.render('index', { title: 'Mikan Express Page' });
 });
 
