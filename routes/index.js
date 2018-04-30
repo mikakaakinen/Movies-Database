868b610286bd9d3c924dacbea04bfabba4ccd0b8
Mika Kaakinen <mika.kaakinen@gmail.com>
2018-04-30 10:07:16 +0300

    Help-7

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Hello Application');
   res.render('index', { title: 'Mikan Express Page' });
+});
+
+router.post('/', function(req, res, next) {
+var query = req.body.nimi_kentta; 
+db.collection("Elokuvat kokoelma").find(query).toArray(function(err, docs) {
+    if (err) throw err;
+    // renders index.pug
+    res.render('index', docs);
+  });
 });
 
 module.exports = router;
