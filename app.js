1df2c4faf1c84946370e29fe1a410272f0d919a8
Mika Kaakinen <mika.kaakinen@gmail.com>
2018-05-02 09:13:48 +0300

    Help-20

--- app.js
+++ app.js
@@ -43,23 +43,18 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
- 
-// Connection URL
-const url = 'mongodb://localhost:27017';
- 
-// Database Name
-const dbName = 'Elokuvat_database';
- 
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
- 
-  const db = client.db(dbName);
- 
-  client.close();
-});
+//Import the mongoose module
+var mongoose = require('mongoose');
+
+//Set up default mongoose connection
+
+mongoose.connect('mongodb://mika.kaakinen@gmail.com:vzFoHOkyWDMJTFZY6AcO@ds014808.mlab.com:14808/elokuvat_database');
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -6,20 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mikan Express Page' });
 });
 
+var result_from_mongo = [];
 
 router.post('/', function(req, res, next) {
 var query = req.body.nimi_kentta; 
-const findDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('Elokuvat kokoelma');
-  // Find some documents
-  collection.find(query).toArray(function(err, docs) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(docs);
-    res.render('index', {'results': docs });
+db.collection("elokuvat_kokoelma").find(query).toArray(function(err, docs) {
+    if (err) throw err;
+    //Push result onto results_array
+    result_from_mongo.push(docs); 
+    // renders index.pug
+    res.render('index', {'results': result_from_mongo});
   });
-};
 });
 
 module.exports = router;
