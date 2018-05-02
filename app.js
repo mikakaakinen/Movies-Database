79a6bfaa306307c05e94d1be722d0598f2595200
Mika Kaakinen <mika.kaakinen@gmail.com>
2018-05-02 13:09:04 +0300

    Help-25

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ var mongoose = require('mongoose');
 
 //Set up default mongoose connection
 
-mongoose.connect('mongodb://MikaKaakinen:mdu3PxxO81@ds014808.mlab.com:14808/elokuvat_database');
+mongoose.connect('mongodb://MK:o5lglNOWuF@ds014808.mlab.com:14808/elokuvat_database');
 // Get Mongoose to use the global promise library
 mongoose.Promise = global.Promise;
 //Get the default connection
