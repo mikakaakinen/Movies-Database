7107461818ca88ebf6881d66172198947e0e6d3e
Mika Kaakinen <mika.kaakinen@gmail.com>
2018-05-04 16:49:45 +0300

    Help-67

--- routes/index.js
+++ routes/index.js
@@ -35,7 +35,7 @@ var query = req.body.nimi_kentta;
 // object of the user
 //res.render('index',docs);
 //});
-Elokuvat.find({}, function(err, user) {
+Elokuvat.find({"elokuvan_nimi": "query"}, function(err, user) {
 if (err) throw err;
 // object of the user
 res.render('index', {docs: user});
