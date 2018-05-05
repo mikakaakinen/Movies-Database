95f86746e9c766133ae0b7aec3971d0d0010b1fc
Mika Kaakinen <mika.kaakinen@gmail.com>
2018-05-05 11:45:21 +0300

    Help-78

--- routes/index.js
+++ routes/index.js
@@ -30,10 +30,10 @@ newUser.save(function(err) {
 router.post('/', function(req, res, next) {
 console.log("'Post router operates!'");
 var query = req.body.nimi_kentta;
-Elokuvat.find({Elokuvan_nimi:query}).toArray(function(err, user) {
+Elokuvat.find({Elokuvan_nimi:query},(function(err, user) {
 if (err) throw err;
 // object of the user
 res.render('index', {docs: user});
-});
+}));
 });
 module.exports = router;
