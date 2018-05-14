70063e251c85f62b57974e9651b9cd40b0e29d0a
Mika Kaakinen <mika.kaakinen@gmail.com>
2018-05-14 07:13:44 +0300

    Help-183

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var pug = require('pug');
+
+// Compile the source code
+var compiledFunction = pug.compileFile('layout.pug');
 
 var app = express();
 
